457170e2df44747a0d9c5b955c8686ee4453fe51
valentina <145261580+valebonato@users.noreply.github.com>
2024-08-08 16:28:28 +0200

    Delete example_bash_for_merging.sh

--- example_bash_for_merging.sh
+++
@@ -1,7 +0,0 @@
-
-startYear="1980"
-endYear="1984"
-inputDirRoot="/scratch-shared/vbonato/dynqual_simulations/w5e5/existing/1980-1984/"
-outputDir="/scratch-shared/edwinaha/dynqual_simulations/w5e5/existing/1980-1984/global/"
-
-sbatch --export inputDirRoot=${inputDirRoot},outputDir=${outputDir},startYear=${startYear},endYear=${endYear} slurm_job_for_merging.sh
